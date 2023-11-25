a2c8d102f7a20c8c2c4eb85eb18f5075be141cd1
matheusjean28 <matheusjean28@gmail.com>
2023-11-25 19:42:24 -0300

    centralize main div

---
+++ src/Components/ChatMain.jsx
@@ -0,0 +1,12 @@
+import '../Styles/ChatMain.css'
+
+export default function ChatMain() {
+    return (
+        <>
+            <div className="ChatMainConteiner">
+            <p>is running</p>
+            </div>
+
+        </>
+    )
+}
