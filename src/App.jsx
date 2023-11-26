6c2f5412fe20e7ec809d550ac44f446a2ebbe002
matheusjean28 <matheusjean28@gmail.com>
2023-11-26 14:55:26 -0300

    created a global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
+import AppContext from './Context/AppContext'
+import { useContext } from 'react'
 import ChatMain from './Components/ChatMain'
 import Profile from './Components/Profile'
 import './App.css'
 
 function App() {
-
   return (
     <>
+    {/* <AppContext.Provider value={}> */}
     <div className="MainGrid">  
       <ChatMain />
       <Profile />
     </div>
+    {/* </AppContext.Provider> */}
     </>
   )
 }
--- src/Components/RenderMessage.jsx
+++ src/Components/RenderMessage.jsx
@@ -1,14 +1,17 @@
+import { useContext } from "react";
+import AppContext from '../Context/AppContext'
 import useWebSocket from "react-use-websocket";
 import '../Styles/RenderMessage.css'
 export default function RenderMessage() {
   const { lastJsonMessage, readyState } = useWebSocket(
     "ws://localhost:5146"
   );
- 
+
 
   return (
     <>
       <div className="RenderMessageConteiner">
+        
         <h4>You: {lastJsonMessage && lastJsonMessage.text}</h4>
       </div>
     </>
