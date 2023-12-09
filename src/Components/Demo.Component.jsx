69f8ce43944b4d7566d707900939b9dd601dca02
matheusjean28 <matheusjean28@gmail.com>
2023-12-09 04:03:26 -0300

    more than one user can connect now

--- src/Components/Demo.Component.jsx
+++ src/Components/Demo.Component.jsx
@@ -5,16 +5,26 @@ const WebSocketDemo = () => {
   const [connection, setConnection] = useState(null);
 
   useEffect(() => {
-
     const newConnection = new signalR.HubConnectionBuilder()
       .withUrl("http://localhost:5178/chatHub")
       .configureLogging(signalR.LogLevel.Information)
       .build();
 
+    newConnection.on("ReceberMensagem", (usuario, mensagem) => {
+      console.log(`${usuario}: ${mensagem}`);
+    });
+
     setConnection(newConnection);
 
-    return () => {
+    newConnection.start()
+      .then(() => {
+        console.log('Conectado ao hub!');
+      })
+      .catch((error) => {
+        console.error('Erro ao conectar ao hub:', error);
+      });
 
+    return () => {
       if (newConnection) {
         newConnection.stop();
       }
@@ -22,22 +32,23 @@ const WebSocketDemo = () => {
   }, []);
 
   useEffect(() => {
-
     if (connection) {
-      connection
-        .start()
-        .then(() => {
-          console.log('Conectado ao hub!');
-        })
-        .catch((error) => {
-          console.error('Erro ao conectar ao hub:', error);
-        });
+      connection.onclose((error) => {
+        console.error('Conexão fechada:', error);
+      });
+
+      connection.onreconnecting(() => {
+        console.log('Reconectando...');
+      });
+
+      connection.onreconnected(() => {
+        console.log('Reconectado!');
+      });
     }
   }, [connection]);
 
   const enviarMensagem = () => {
-
-    if (connection) {
+    if (connection && connection.state === signalR.HubConnectionState.Connected) {
       connection
         .invoke('EnviarMensagem', 'matheus', 'Olá, mundo!')
         .catch((error) => {
