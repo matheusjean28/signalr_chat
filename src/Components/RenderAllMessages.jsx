1e9d232cbdddfa0a49a1f5ae84efbeda7293c7ee
matheusjean28 <matheusjean28@gmail.com>
2023-12-12 21:00:53 -0300

    resolve renders side

--- src/Components/RenderAllMessages.jsx
+++ src/Components/RenderAllMessages.jsx
@@ -4,7 +4,7 @@ import AppContext from "../Context/AppContext";
 import "../Styles/RenderMessage.css";
 
 const RenderAllMessages = () => {
-  const { connection, recivedMessages, setRecivedMessages } =
+  const { connection, recivedMessages, setRecivedMessages, username } =
     useContext(AppContext);
   const [mensagens, setMensagens] = useState([]);
 
@@ -22,11 +22,25 @@ const RenderAllMessages = () => {
     return () => {};
   }, [connection]);
 
+  //if last message == you then just render usermessa
   return (
     <ul className="RenderMessageConteiner">
       {mensagens.map(({ user, userMessage, index }) => (
-        <li key={index}>
-          {user} : {userMessage}
+        <li
+          key={index}
+          className={user === username ? "CurrentUser" : "OtherUser"}
+        >
+          {user === username ? (
+            <div className="CurrentUserSide">
+              <h4 className="RenderMessageYou"> You</h4>
+              <h5 className="RenderMessageYou">{userMessage}</h5>
+            </div>
+          ) : (
+            <div className="OtherUserSide">
+              <h4 className="RenderMessageOther"> {user}</h4>
+              <h5 className="RenderMessageOther">{userMessage}</h5>
+            </div>
+          )}
         </li>
       ))}
       <ChatMain />
