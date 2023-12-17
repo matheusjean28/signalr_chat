8dfe721089da9024d7b7d745a4d8078d7d425469
matheusjean28 <matheusjean28@gmail.com>
2023-12-17 14:57:11 -0300

    list of avaliables chat rooms to join

--- src/Components/ChatRooms.jsx
+++ src/Components/ChatRooms.jsx
@@ -2,11 +2,221 @@ import '../Styles/ChatRooms.css'
 import React from 'react'
 
 const ChatRooms = () => {
-    return( 
-        <React.Fragment>
-            <p>all is alrigt</p>
+    return (
+        <div className="ChatRoomConteiner">
+            <div className="ChatRoomLeft">
+                <h2>JOIN ROOM!</h2>
+                <p>Chose a room and join to get a conversation!</p>
+                <ul className="ChatListUl">
+                    <li className="ChatListLi">
 
-        </React.Fragment>
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li>
+                    <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li>
+                    <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li> <li className="ChatListLi">
+
+                        {/* //it render a list with each picprofile from user that are in current chat
+//if this list contains more than 3 users, them show a 3+ user's */}
+                        <ul className='UlUserInRoom'>
+                            {/* make an if here and check if array of users at room is >= 3 */}
+                            <li className=' LiUserInRoom'>
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                                <img className='ImgUserInRoom' src="src/assets/astronaut.svg" alt="" />
+                            </li>
+                        </ul>
+                        <p>
+                            10 Online's
+                        </p>
+                        <p>Science</p>
+                        <button className='ChatListJoinButton' onClick={(e) => {
+                            e.preventDefault();
+                        }}> JOIN</button>
+
+                    </li>
+                </ul>
+            </div>
+            <div className="">
+                <p>ok</p>
+            </div>
+        </div>
     )
 }
 
