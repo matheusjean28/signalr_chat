280c122555b6b31137e80a2c146ea6bb0e43e189
matheusjean28 <matheusjean28@gmail.com>
2024-03-19 12:48:04 -0300

    start to implement chatConfig to show chat informations and ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,208 +1,209 @@
-import { React, useEffect, useState, signalR } from "./imports";
+import { React, useEffect, useState, signalR } from './imports'
 
-import "./App.css";
-import AppContext from "./Context/AppContext";
-import ChatRooms from "./Components/ChatRooms";
-import RenderAllMessages from "./Components/RenderAllMessages";
-import Login from "./Components/LoginComponents/Login";
-import ProfileSettings from "./Components/ProfileSettings";
-import CreateRoom from "./Components/CreateRoom";
-import ScreenErrorComponent from "./Components/ErrorComponent/ScreenErrorComponent";
-import ConnectionStatus from "./Components/ConnectionStatus/ConnectionStatus";
-import ErrorPopup from "./Components/ErrorComponent/ErrorPopup";
-import ChatConfig from "./Components/ChatConfig/ChatConfig";
+import './App.css'
+import AppContext from './Context/AppContext'
+import ChatRooms from './Components/ChatRooms'
+import RenderAllMessages from './Components/RenderAllMessages'
+import Login from './Components/LoginComponents/Login'
+import ProfileSettings from './Components/ProfileSettings'
+import CreateRoom from './Components/CreateRoom'
+import ScreenErrorComponent from './Components/ErrorComponent/ScreenErrorComponent'
+import ConnectionStatus from './Components/ConnectionStatus/ConnectionStatus'
+import ErrorPopup from './Components/ErrorComponent/ErrorPopup'
+import ChatConfig from './Components/ChatConfig/ChatConfig'
 
 //icons
-import { Bot, ImageOff } from "lucide-react";
-import GlobalConteiner from "./Components/GlobalConteiner/GlobalConteiner";
+import { Bot, ImageOff } from 'lucide-react'
+import GlobalConteiner from './Components/GlobalConteiner/GlobalConteiner'
 
 function App() {
-  //make a file to handle this 'consts'
-  const [username, setUsername] = useState("");
-  const [connection, setConnection] = useState(null);
-  const [stateConnection, setStateConnection] = useState(null);
-  const [recivedMessages, setRecivedMessages] = useState([]);
-  const [login, setLogin] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const [isLoged, setIsLoged] = useState(false);
-  const [isInARoom, setIsInARoom] = useState(false);
-  const [currentChat, setCurrentChat] = useState("");
-  const [chatName, setChatName] = useState("");
-  const [isEditing, setIsEditing] = useState(false);
-  const [isCreatingARoom, setCreatingARoom] = useState(false);
-  const [userInfo, setUserInfo] = useState({
-    Id: "",
-    UserName: username,
-    picProfile: "src/assets/editar.png ",
-    Gender: "asdf",
-    bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-  });
+    //make a file to handle this 'consts'
+    const [username, setUsername] = useState('')
+    const [connection, setConnection] = useState(null)
+    const [stateConnection, setStateConnection] = useState(null)
+    const [recivedMessages, setRecivedMessages] = useState([])
+    const [login, setLogin] = useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+    const [isLoged, setIsLoged] = useState(false)
+    const [isInARoom, setIsInARoom] = useState(false)
+    const [currentChat, setCurrentChat] = useState('')
+    const [chatName, setChatName] = useState('')
+    const [isEditing, setIsEditing] = useState(false)
+    const [isCreatingARoom, setCreatingARoom] = useState(false)
+    const [userInfo, setUserInfo] = useState({
+        Id: '',
+        UserName: username,
+        picProfile: 'src/assets/editar.png ',
+        Gender: 'asdf',
+        bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    })
 
-  const [isEditingChat, setIsEditingChat] = useState(false);
+    const [isEditingChat, setIsEditingChat] = useState(false)
 
-  const [renderError, setRenderError] = useState(false);
-  const [popMessage, setPopMessage] = useState("");
+    const [renderError, setRenderError] = useState(false)
+    const [popMessage, setPopMessage] = useState('')
 
-  const [chatInformation, setChatInformation] = useState({});
+    const [chatInformation, setChatInformation] = useState({})
 
-  useEffect(() => {
-    const newConnection = new signalR.HubConnectionBuilder()
-      .withUrl("http://localhost:5178/chatHub")
-      .withAutomaticReconnect()
-      .build();
+    useEffect(() => {
+        const newConnection = new signalR.HubConnectionBuilder()
+            .withUrl('http://localhost:5178/chatHub')
+            .withAutomaticReconnect()
+            .build()
 
-    newConnection.onreconnecting(() => {
-      setStateConnection("Reconnecting");
-    });
+        newConnection.onreconnecting(() => {
+            setStateConnection('Reconnecting')
+        })
 
-    newConnection.onreconnected(() => {
-      setStateConnection("Connected");
-    });
+        newConnection.onreconnected(() => {
+            setStateConnection('Connected')
+        })
 
-    newConnection.onclose(async () => {
-      console.log("call reconnection cause disconnected");
-      await handleReconnection(newConnection);
-    });
+        newConnection.onclose(async () => {
+            console.log('call reconnection cause disconnected')
+            await handleReconnection(newConnection)
+        })
 
-    newConnection
-      .start()
-      .then(() => {
-        setConnection(newConnection);
-        setStateConnection("Connected");
-      })
-      .catch((error) => {
-        console.error(error);
-        setRenderError(true);
-        setPopMessage("Failed to connect to the server.");
-        setTimeout(() => handleReconnection(newConnection), 5000);
-      });
+        newConnection
+            .start()
+            .then(() => {
+                setConnection(newConnection)
+                setStateConnection('Connected')
+            })
+            .catch((error) => {
+                console.error(error)
+                setRenderError(true)
+                setPopMessage('Failed to connect to the server.')
+                setTimeout(() => handleReconnection(newConnection), 5000)
+            })
 
-    return () => {};
-  }, []);
+        return () => {}
+    }, [])
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setPopMessage("");
-    }, 9000);
-    return () => clearTimeout(timer);
-  }, [popMessage]);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setPopMessage('')
+        }, 9000)
+        return () => clearTimeout(timer)
+    }, [popMessage])
 
-  //leave this to first, will set as null
-  useEffect(() => {
-    setStateConnection(connection?.state);
-  }, [connection]);
+    //leave this to first, will set as null
+    useEffect(() => {
+        setStateConnection(connection?.state)
+    }, [connection])
 
-  //called when connection goes wrong
-  const handleReconnection = async (connection) => {
-    try {
-      await connection.start();
-      if (connection.connection.state === "Connected") {
-        setRenderError(false);
-        setPopMessage("");
-      }
-    } catch (error) {
-      console.error("Error reconnecting:", error);
-      setPopMessage(error);
-      setRenderError(true);
-      setPopMessage("Failed to reconnect.");
-      setTimeout(handleReconnection, 1000);
+    //called when connection goes wrong
+    const handleReconnection = async (connection) => {
+        try {
+            await connection.start()
+            if (connection.connection.state === 'Connected') {
+                setRenderError(false)
+                setPopMessage('')
+            }
+        } catch (error) {
+            console.error('Error reconnecting:', error)
+            setPopMessage(error)
+            setRenderError(true)
+            setPopMessage('Failed to reconnect.')
+            setTimeout(handleReconnection, 1000)
+        }
     }
-  };
 
-  try {
-    return (
-      <AppContext.Provider
-        value={{
-          chatInformation,
-          setChatInformation,
-          stateConnection,
-          setStateConnection,
-          isCreatingARoom,
-          setCreatingARoom,
-          currentChat,
-          setCurrentChat,
-          isEditing,
-          setIsEditing,
-          userInfo,
-          setUserInfo,
-          chatName,
-          setChatName,
-          isInARoom,
-          setIsInARoom,
-          isLoged,
-          setIsLoged,
-          login,
-          setLogin,
-          connection,
-          setConnection,
-          username,
-          setUsername,
-          recivedMessages,
-          setRecivedMessages,
-          renderError,
-          setRenderError,
-          popMessage,
-          setPopMessage,
-        }}
-      >
-        {popMessage && <ErrorPopup />}
-        {isLoged ? <ConnectionStatus /> : ""}
-        {renderError ? (
-          <ScreenErrorComponent popMessage={popMessage} />
-        ) : isLoged ? (
-          //yes{}
-          isCreatingARoom ? (
-            //yes
-            <CreateRoom />
-          ) : isInARoom ? (
-            isEditingChat ? (
-              //yes
+    try {
+        return (
+            <AppContext.Provider
+                value={{
+                    chatInformation,
+                    setChatInformation,
+                    stateConnection,
+                    setStateConnection,
+                    isCreatingARoom,
+                    setCreatingARoom,
+                    currentChat,
+                    setCurrentChat,
+                    isEditing,
+                    setIsEditing,
+                    userInfo,
+                    setUserInfo,
+                    chatName,
+                    setChatName,
+                    isInARoom,
+                    setIsInARoom,
+                    isLoged,
+                    setIsLoged,
+                    login,
+                    setLogin,
+                    connection,
+                    setConnection,
+                    username,
+                    setUsername,
+                    recivedMessages,
+                    setRecivedMessages,
+                    renderError,
+                    setRenderError,
+                    popMessage,
+                    setPopMessage,
+                }}
+            >
+                {popMessage && <ErrorPopup />}
+                {isLoged ? <ConnectionStatus /> : ''}
+                {renderError ? (
+                    <ScreenErrorComponent popMessage={popMessage} />
+                ) : isLoged ? (
+                    //yes{}
+                    isCreatingARoom ? (
+                        //yes
+                        <CreateRoom />
+                    ) : isInARoom ? (
+                        isEditingChat ? (
+                            //yes
 
-              <GlobalConteiner
-                chatInformation={chatInformation}
-                setChatInformation={setChatInformation}
-                chatName={chatName}
-                currentChat={currentChat}
-                setIsEditingChat={setIsEditingChat}
-                isEditingChat={isEditingChat}
-              />
-            ) : (
-              <GlobalConteiner
-                chatInformation={chatInformation}
-                setChatInformation={setChatInformation}
-                chatName={chatName}
-                currentChat={currentChat}
-                setIsEditingChat={setIsEditingChat}
-                isEditingChat={isEditingChat}
-              />
-            )
-          ) : (
-            <ChatRooms />
-          )
-        ) : (
-          <Login />
-        )}
-      </AppContext.Provider>
-    );
-  } catch (error) {
-    console.log(error);
-    return (
-      <AppContext.Provider
-        value={{
-          setRenderError,
-          setPopMessage,
-          renderError,
-          popMessage,
-        }}
-      >
-        setRenderError(true); setPopMessage("Failed to render main component.");
-        <ScreenErrorComponent />
-      </AppContext.Provider>
-    );
-  }
+                            <GlobalConteiner
+                                chatInformation={chatInformation}
+                                setChatInformation={setChatInformation}
+                                chatName={chatName}
+                                currentChat={currentChat}
+                                setIsEditingChat={setIsEditingChat}
+                                isEditingChat={isEditingChat}
+                            />
+                        ) : (
+                            <GlobalConteiner
+                                chatInformation={chatInformation}
+                                setChatInformation={setChatInformation}
+                                chatName={chatName}
+                                currentChat={currentChat}
+                                setIsEditingChat={setIsEditingChat}
+                                isEditingChat={isEditingChat}
+                            />
+                        )
+                    ) : (
+                        <ChatRooms />
+                    )
+                ) : (
+                    <Login />
+                )}
+            </AppContext.Provider>
+        )
+    } catch (error) {
+        console.log(error)
+        return (
+            <AppContext.Provider
+                value={{
+                    setRenderError,
+                    setPopMessage,
+                    renderError,
+                    popMessage,
+                }}
+            >
+                setRenderError(true); setPopMessage("Failed to render main
+                component.");
+                <ScreenErrorComponent />
+            </AppContext.Provider>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/Components/ChatConfig/ChatConfig.jsx
+++ src/Components/ChatConfig/ChatConfig.jsx
@@ -1,47 +1,68 @@
-import "../../Styles/ChatConfigtyles.css";
-import { Bot, ImageOff, ChevronLeft } from "lucide-react";
+import '../../Styles/ChatConfigtyles.css'
+import { Bot, ImageOff, ChevronLeft, AtSignIcon } from 'lucide-react'
 
 const ChatConfig = ({
-  currentChat,
-  setIsEditingChat,
-  isEditingChat,
-  chatInformation,
-  setChatInformation,
+    currentChat,
+    setIsEditingChat,
+    isEditingChat,
+    chatInformation,
+    setChatInformation,
 }) => {
-  console.log(chatInformation);
-  return (
-    <div className="ChatConfigConteiner">
-      {/* header button */}
-      <button
-        className="handleCloseBtn"
-        onClick={(e) => {
-          e.preventDefault();
-          setIsEditingChat(!isEditingChat);
-        }}
-      >
-        <span>
-          <ChevronLeft size={20} />
-        </span>
-        <h2>Back</h2>
-      </button>
+    const {chatName } = chatInformation;
+    console.log(chatInformation)
+    return (
+        <div className="ChatConfigConteiner">
+            {/* header button */}
+            <div className="leftInfoConteiner">
+              <span className='chatNameSpan'>
+                <h4>{chatName}</h4>
+                <AtSignIcon size={12}/>
+              </span>
 
-      <div className="infoConteiner">
-        <h4>Manegers:</h4>
-        <ul className="ulInfoManegers">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <li className="liInfoManagers">
-              <p className="infoContent" key={index}>{index}</p>
-              <p className="infoContent managersName">Matth</p>
-              <p className="infoContent cargo">manager</p>
+            </div>
 
+            {/* card info about manager */}
+            <div className="infoConteiner">
+                <h4>Manegers:</h4>
+                <ul className="ulInfoManegers ">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                        <li className="liInfoManagers">
+                            <p className="infoContent" key={index}>
+                                {}
+                            </p>
+                            <p className="infoContent managersName">Matth</p>
+                            <p className="infoContent cargo">manager</p>
+                        </li>
+                    ))}
+                </ul>
+                <h4 className='currentUsersCards'>Current Users:</h4>
+                <ul className="ulInfoManegers ">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                        <li className="liInfoManagers">
+                            <p className="infoContent" key={index}>
+                                {}
+                            </p>
+                            <p className="infoContent managersName">Matth</p>
+                            <p className="infoContent cargo">manager</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
+            <button
+                className="handleCloseBtn"
+                onClick={(e) => {
+                    e.preventDefault()
+                    setIsEditingChat(!isEditingChat)
+                }}
+            >
+                <span>
+                    <ChevronLeft size={20} />
+                </span>
+                <h2>Back</h2>
+            </button>
+        </div>
+    )
+}
 
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default ChatConfig;
+export default ChatConfig
